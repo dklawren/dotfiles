e9422d53135a5ea085e4cb905351d5b13e6fb372
David Lawrence <dkl@mozilla.com>
2023-03-22 12:28:09 -0400

    Initial checkin of setup script for silverblue host

---
+++ .bin/silverblue.sh
@@ -0,0 +1,52 @@
+# Installation script for Silverblue host
+
+# Initialize flatpak support
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+flatpak remote-add --if-not-exists gnome-nightly https://nightly.gnome.org/gnome-nightly.flatpakrepo
+flatpak update --appstream
+flatpak update
+
+# Install flatpak apps
+flatpak install -y --or-update \
+  com.github.alexkdeveloper.somafm \
+  com.github.johnfactotum.Foliate \
+  com.github.tchx84.Flatseal \
+  com.gitlab.newsflash \
+  com.logseq.Logseq \
+  com.mattjakeman.ExtensionManager \
+  com.slack.Slack \
+  de.haeckerfelix.Shortwave \
+  org.gnome.World.PikaBackup \
+  org.mozilla.Thunderbird \
+  org.mozilla.firefox \
+  org.videolan.VLC \
+  us.zoom.Zoom
+
+# Install packages
+rpm-ostree upgrade
+rpm-ostree remove -y \
+  firefox \
+  firefox-langpacks
+rpm-ostree install -y \
+  cascadia-code-fonts\
+  distrobox \
+  exa \
+  fzf \
+  git \
+  git-extras \
+  gnome-tweaks \
+  htop \
+  langpacks-en \
+  moby-engine \
+  neovim \
+  nodejs \
+  onedrive \
+  podman-compose \
+  ripgrep \
+  syncthing \
+  sysstat \
+  tlp \
+  tlp-rdw \
+  util-linux-user \
+  zsh
