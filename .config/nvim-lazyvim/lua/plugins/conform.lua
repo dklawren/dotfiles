c08eb5b8182348da4fa4eb5b59c5cbd0e7d4550b
David Lawrence <dkl@mozilla.com>
2023-12-13 20:39:52 -0500

    Break plugins up into individual lua files

---
+++ .config/nvim-lazyvim/lua/plugins/conform.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = {
+        lua = { "stylua" },
+        perl = { "perltidy" },
+        python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
+      }
+      opts.log_level = vim.log.levels.ERROR
+      opts.notify_on_error = true
+    end,
+  },
+}
---
+++ .config/nvim-lazyvim/lua/plugins/project.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "coffebar/neovim-project",
+    opts = {
+      projects = { -- define project roots
+        "~/devel/github/mozilla/bmo/upstream/*",
+        "~/devel/github/mozilla/lando-ui",
+        "~/devel/github/mozilla/lando-api/*",
+        "~/devel/github/mozilla/phabricator/*",
+        "~/devel/github/dklawren/bmo/*",
+        "~/local-devel/*",
+        "~/.config/*",
+      },
+    },
+    init = function()
+      -- enable saving the state of plugins in the session
+      vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+    end,
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+      { "Shatur/neovim-session-manager" },
+    },
+    lazy = false,
+    priority = 100,
+  },
+}
