357f35c158df5191e31d0588639f2551aae73855
David Lawrence <dkl@mozilla.com>
2024-01-20 17:00:00 -0500

    telescope fzf-native

--- .config/nvim-lazyvim/lua/plugins/telescope.lua
+++ .config/nvim-lazyvim/lua/plugins/telescope.lua
@@ -4,4 +4,12 @@ return {
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   },
   "KilloPillers/telescope-media-files.nvim",
+  {
+    "telescope.nvim",
+    dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
+    build = "make",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
 }
