6d8fc90bc4bd7a0ee85da94e9cce70aab9fca484
David Lawrence <dkl@mozilla.com>
2025-02-17 17:44:53 -0500

    new sawy config

--- .local/bin/silverblue.sh
+++ .local/bin/silverblue.sh
@@ -12,12 +12,9 @@ flatpak install -y --or-update \
   ca.andyholmes.Valent \
   ca.desrt.dconf-editor \
   com.calibre_ebook.calibre \
-  com.github.alexkdeveloper.somafm \
+  com.github.alexkdeveloper.somafm \ com.github.johnfactotum.Foliate \
   com.github.d4nj1.tlpui \
-  com.github.johnfactotum.Foliate \
-  com.github.neithern.g4music \
   com.github.tchx84.Flatseal \
-  com.gitlab.newsflash \
   com.google.Chrome \
   com.jeffser.Alpaca \
   com.mattjakeman.ExtensionManager \
@@ -25,13 +22,8 @@ flatpak install -y --or-update \
   com.transmissionbt.Transmission \
   com.ultimaker.cura \
   com.usebottles.bottles \
-  com.vivaldi.Vivaldi \
-  dev.bragefuglseth.Keypunch \
-  fr.handbrake.ghb \
   io.github.alexkdeveloper.radio \
   io.github.flattool.Warehouse \
-  io.github.vikdevelop.SaveDesktop \
-  io.github.zen_browser.zen \
   io.gitlab.adhami3310.Impression \
   io.gitlab.librewolf-community \
   io.missioncenter.MissionCenter \
@@ -39,14 +31,7 @@ flatpak install -y --or-update \
   it.mijorus.gearlever \
   md.obsidian.Obsidian \
   me.iepure.devtoolbox \
-  net.codelogistics.webapps \
-  net.waterfox.waterfox \
-  one.ablaze.floorp \
   org.cockpit_project.CockpitClient \
-  org.fedoraproject.MediaWriter \
-  org.gnome.baobab \
-  org.gnome.Calculator \
-  org.gnome.Characters \
   org.gnome.Cheese \
   org.gnome.clocks \
   org.gnome.Connections \
@@ -74,38 +59,75 @@ flatpak install -y --or-update \
 flatpak override --user --env=MOZ_ENABLE_WAYLAND=1 org.mozilla.Thunderbird
 
 # Firefox nightly
-flatpak install https://gitlab.com/projects261/firefox-nightly-flatpak/-/raw/main/firefox-nightly.flatpakref
+# flatpak install https://gitlab.com/projects261/firefox-nightly-flatpak/-/raw/main/firefox-nightly.flatpakref
+
+# Install Docker CE
+if [ ! -f /etc/yum.repos.d/docker-ce.repo ]; then
+  sudo tee -a /etc/yum.repos.d/docker-ce.repo << EOM
+[docker-ce-stable]
+name=Docker CE Stable - x86_64
+baseurl=https://download.docker.com/linux/fedora/41/x86_64/stable
+enabled=1
+gpgcheck=1
+gpgkey=https://download.docker.com/linux/fedora/gpg
+EOM
+fi
 
 # Install packages
 rpm-ostree upgrade
 rpm-ostree remove -y \
   firefox \
   firefox-langpacks
-rpm-ostree install -y \
-  cascadia-code-fonts\
+rpm-ostree install -y --allow-inactive --idempotent \
+  alacritty \
+  ansible \
+  btop \
+  cascadia-code-fonts \
+  containerd.io \
   distrobox \
-  exa \
+  docker-buildx-plugin \
+  docker-ce \
+  docker-ce-cli \
+  docker-compose-plugin \
   fira-code-fonts \
   fzf \
   git \
   git-extras \
   gnome-tweaks \
+  grim \
+  grimshot \
   htop \
-  moby-engine \
+  mako \
+  mousepad \
   neovim \
   nodejs \
   onedrive \
   openssl \
   pandoc \
+  playerctl \
   podman-compose \
   polkit \
   ripgrep \
+  rofi \
+  slurp \
+  swaybg \
+  swayidle \
+  swayimg \
+  swaylock \
+  SwayNotificationCenter \
+  sway-wallpapers \
   syncthing \
   sysstat \
+  thunar \
   tlp \
   tlp-rdw \
   tmux \
+  udiskie \
+  waybar \
   waydroid \
+  wf-recorder \
+  wireplumber \
+  wlsunset \
   xdg-desktop-portal-wlr \
   xorg-x11-server-Xwayland \
   zsh \
@@ -118,7 +140,7 @@ sudo systemctl --now enable docker.service
 systemctl --user --now enable syncthing.service
 
 # tlp
-sudo cp $HOME/.local/bin/files/tlp.conf /etc/tlp.conf
+sudo cp "$HOME/.local/bin/files/tlp.conf" /etc/tlp.conf
 sudo systemctl --now enable tlp.service
 sudo systemctl stop power-profiles-daemon.service
 sudo systemctl disable power-profiles-daemon.service
