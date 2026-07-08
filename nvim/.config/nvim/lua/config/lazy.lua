811560943754cb73778f94d022a2294e5c6907f3
David Lawrence <dkl@mozilla.com>
2026-07-07 21:25:04 -0400

    Added highlighting for perl-lsp

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -51,9 +51,22 @@ require("lazy").setup({
   },
 })
 
+-- Perl LSP - Need to find a better place for this
 vim.lsp.config["perl-lsp"] = {
   cmd = { "perl-lsp" },
   filetypes = { "perl" },
   root_markers = { "cpanfile", "Makefile.PL", "Build.PL", ".git" },
 }
 vim.lsp.enable("perl-lsp")
+vim.api.nvim_set_hl(0, "@lsp.type.macro.perl", { link = "Keyword" })      -- has, with, extends
+vim.api.nvim_set_hl(0, "@lsp.type.property.perl", { link = "Identifier" }) -- hash keys
+vim.api.nvim_set_hl(0, "@lsp.type.namespace.perl", { link = "Type" })     -- Foo::Bar
+vim.api.nvim_set_hl(0, "@lsp.type.parameter.perl", { link = "Special" })  -- sub params
+vim.api.nvim_set_hl(0, "@lsp.type.keyword.perl", { link = "Constant" })   -- $self/$class
+vim.api.nvim_set_hl(0, "@lsp.mod.scalar.perl", { fg = "#61afef" })        -- $ blue
+vim.api.nvim_set_hl(0, "@lsp.mod.array.perl", { fg = "#c678dd" })         -- @ purple
+vim.api.nvim_set_hl(0, "@lsp.mod.hash.perl", { fg = "#e5c07b" })          -- % gold
+vim.api.nvim_set_hl(0, "@lsp.mod.modification.perl", { fg = "#e06c75" })  -- writes in red
+vim.api.nvim_set_hl(0, "@lsp.mod.declaration.perl", { bold = true })
+vim.api.nvim_set_hl(0, "@lsp.mod.readonly.perl", { italic = true })
+vim.api.nvim_set_hl(0, "@lsp.mod.defaultLibrary.perl", { italic = true }) -- imported functions
